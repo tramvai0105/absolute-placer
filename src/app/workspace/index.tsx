1b4a5dbdc6f83840f69882846d03b04815320ba7
Timofey Fedorov <tramvai00105@gmail.com>
2024-04-05 07:49:58 +0300

    gram fix

--- src/app/workspace/index.tsx
+++ src/app/workspace/index.tsx
@@ -14,7 +14,7 @@ export default function WorkSpace(){
     return(
         <div className="flex flex-col select-none w-full text-white h-[100vh]">
             <div className="w-full h-12 items-center flex gap-2 pl-2 border-[#7C7A85] border-b-[1px]">
-                <span className="text-[18px] italic border-[1px] rounded-xl px-3">absolute-placer-utill</span>
+                <span className="text-[18px] italic border-[1px] rounded-xl px-3">absolute-placer-util</span>
                 <button onClick={()=>setShowResult(true)} className="border-[2px] border-white hover:bg-[#7C7A85] rounded-md px-2 py-1">Code result</button>
                 <button onClick={()=>setShowConfigMenu(true)} className="border-[1px] border-[#7C7A85] hover:bg-[#7C7A85] rounded-md px-2 py-1">Options</button>
             </div>
